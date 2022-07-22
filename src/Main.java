fdd0bd77c365868e803097cbc4bc0f5fc2540150
lex8483 <lex8483@mail.ru>
2022-07-22 06:02:40 +0300

    dz5.1.2 - Исправлено

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         arr[1] = 2;
         arr[2] = 3;
         for (int i = 0; i < arr.length; i++) {
-            if (i < 2) {
+            if (i < arr.length - 1) {
                 System.out.print(arr[i] + ",");
             } else
                 System.out.print(arr[i]);
@@ -18,7 +18,7 @@ public class Main {
         System.out.println();
         double[] dub = {1.57, 7.654, 9.986};
         for (int i = 0; i < dub.length; i++) {
-            if (i < 2) {
+            if (i < dub.length - 1) {
                 System.out.print(dub[i] + ",");
             } else
                 System.out.print(dub[i]);
@@ -26,27 +26,27 @@ public class Main {
         System.out.println();
         int[] weight = {63, 54, 67, 95, 77};
         for (int i = 0; i < weight.length; i++) {
-            if (i < 4) {
+            if (i < weight.length - 1) {
                 System.out.print(weight[i] + ",");
             } else
                 System.out.print(weight[i]);
         }
         System.out.println("\n-------------------------------------------------------------");
-        for (int i = arr.length-1; i >= 0; i--) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             if (i > 0) {
                 System.out.print(arr[i] + ",");
             } else
                 System.out.print(arr[i]);
         }
         System.out.println();
-        for (int i = dub.length-1; i >= 0; i--) {
+        for (int i = dub.length - 1; i >= 0; i--) {
             if (i > 0) {
                 System.out.print(dub[i] + ",");
             } else
                 System.out.print(dub[i]);
         }
         System.out.println();
-        for (int i = weight.length-1; i >= 0; i--) {
+        for (int i = weight.length - 1; i >= 0; i--) {
             if (i > 0) {
                 System.out.print(weight[i] + ",");
             } else
@@ -57,7 +57,9 @@ public class Main {
             if (!(arr[i] % 2 == 0)) {
                 arr[i] += 1;
             }
-            System.out.printf(arr[i] + " ");
+            if (i < arr.length - 1) {
+                System.out.printf(arr[i] + ",");
+            } else System.out.print(arr[i]);
         }
 
     }
